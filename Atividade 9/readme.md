9c3c4c3fa99e177984c1765d29f339e2d0ba140a
Marcos Vinicius Rodrigues Lima <marcos.viniciusk19@gmail.com>
2023-06-12 17:27:20 -0300

    Atividade 9 - Javascript Parte 1

---
+++ Atividade 9/readme.md
@@ -0,0 +1,67 @@
+# Resumo das Funções
+
+Este projeto contém uma coleção de funções JavaScript para realizar diferentes operações em arrays. Cada função tem uma finalidade específica e pode ser utilizada individualmente ou em conjunto para atender a diferentes necessidades.
+
+A seguir, estão listadas as funções disponíveis neste projeto:
+
+## Funções Disponíveis
+
+### 1. `criarArrayAleatorio(tamanho, min, max)`
+
+Esta função cria um array aleatório com `tamanho` elementos, onde cada elemento está dentro do intervalo especificado por `min` e `max`.
+
+### 2. `questao1(entrada)`
+
+Esta função verifica se uma `entrada` fornecida é um array e exibe uma mensagem indicando se é um array ou não.
+
+### 3. `questao2(entrada)`
+
+Esta função retorna uma cópia do array `entrada` fornecido.
+
+### 4. `questao3(entrada = 1)`
+
+Esta função retorna os primeiros `entrada` elementos de um array aleatório.
+
+### 5. `questao4(entrada = 24)`
+
+Esta função retorna os últimos `entrada` elementos de um array aleatório.
+
+### 6. `questao5(entrada)`
+
+Esta função converte um array em uma string, onde os elementos são separados por vírgulas.
+
+### 7. `questao6(entrada)`
+
+Esta função adiciona um traço (`-`) entre dois números pares consecutivos em uma string.
+
+### 8. `questao7(entrada)`
+
+Esta função encontra o item mais frequente em um array.
+
+### 9. `questao8(entrada)`
+
+Esta função remove itens duplicados de um array, ignorando a diferenciação entre maiúsculas e minúsculas.
+
+### 10. `questao9(array1, array2)`
+
+Esta função calcula a soma dos valores de índice individual de dois arrays fornecidos.
+
+### 11. `questao10()`
+
+Esta função adiciona elementos de um array em outro array.
+
+## Como Utilizar
+
+Cada função está definida em um arquivo JavaScript separado. Para utilizá-las, siga os passos abaixo:
+
+1. Clone o repositório ou faça o download dos arquivos.
+2. Importe a função desejada em seu arquivo JavaScript ou módulo Node.js usando `require`.
+3. Chame a função e forneça os argumentos necessários.
+
+Exemplo de uso:
+
+```javascript
+const script = require('./caminho/para/o/arquivo.js');
+
+const entrada = [1, 2, 3, 4, 5];
+script.questao1(entrada);
---
+++ Atividade 9/script.js
@@ -0,0 +1,140 @@
+// Função para criar um array aleatório com números dentro de um intervalo definido
+criaArrayAleatorio = function(tamanho, min, max) {
+    var array = [];
+  
+    for (var i = 0; i < tamanho; i++) {
+      var numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+      array.push(numeroAleatorio);
+    }
+  
+    return array;
+}
+
+// Exporta a função criaArrayAleatorio para uso em outros módulos
+exports.criarArrayAleatorio = criaArrayAleatorio;
+  
+// Função para verificar se uma entrada é um array
+exports.questao1 = function(entrada) {
+    if (Array.isArray(entrada)) {
+        console.log("É um array!");
+    } else {
+        console.log("Não é um array!");
+    }
+}
+
+// Função para clonar um array
+exports.questao2 = function(entrada){
+    return entrada.slice(); // Utiliza o método slice() para criar uma cópia do array
+}
+
+// Função para retornar os primeiros 'entrada' elementos de um array aleatório
+exports.questao3 = function(entrada = 1){
+    var arrayAleatorio = criaArrayAleatorio(25, 0, 100)
+    console.log(arrayAleatorio);
+    var array = [];
+    for(var i = 0; i < entrada; i++){
+        array.push(arrayAleatorio[i]);
+    }
+    return array;
+}
+
+// Função para retornar os últimos 'entrada' elementos de um array aleatório
+exports.questao4 = function(entrada = 24){
+    var arrayAleatorio = criarArrayAleatorio(25, 0, 100)
+    console.log(arrayAleatorio);
+    var array = [];
+    for(var i = entrada; i < 25; i++){
+        array.push(arrayAleatorio[i]);
+    }
+    return array;
+}
+
+// Função para converter um array em uma string separada por vírgulas
+exports.questao5 = function(entrada){
+    return entrada.join();
+}
+
+// Função para adicionar um traço '-' entre dois números pares consecutivos em uma string
+exports.questao6 = function(entrada){
+    entrada = entrada.toString();
+    entrada = entrada.split('');
+    var traco = 0;
+
+    for(var i = 0; i < entrada.length; i++){
+        if(entrada[i] % 2 == 0){
+            traco++;
+        }else{
+            traco = 0;
+        }
+
+        if(traco == 2){
+            entrada.splice(i, 0, '-');
+            traco = 0;
+        }
+    }
+    entrada = entrada.join();
+    entrada = entrada.replace(/,/g, "");
+    return entrada;
+}
+
+// Função para encontrar o item mais frequente em um array
+exports.questao7 = function(entrada){
+    var frequencia = {};
+    var itemMaisFrequente;
+    var frequenciaMaxima = 0;
+
+    for(var i = 0; i < entrada.length; i++){
+        var item = entrada[i];
+        frequencia[item] = (frequencia[item] || 0) + 1;
+
+        if(frequencia[item] > frequenciaMaxima){
+            itemMaisFrequente = item;
+            frequenciaMaxima = frequencia[item];
+        }
+    }
+
+    return itemMaisFrequente;
+}
+
+// Função para remover itens duplicados de um array, ignorando a diferenciação entre maiúsculas e minúsculas
+exports.questao8 = function(entrada){
+    var array = entrada.filter((item, index) => {
+        return entrada.indexOf(item.toLowerCase()) === index;
+    });
+
+    return array;
+}
+
+// Função para calcular a soma dos valores de índice individual de dois arrays
+exports.questao9 = function(array1, array2){
+    var array = [];
+
+    if(array1.length > array2.length){
+        for(var i = array2.length; i < array1.length;i++){
+            array2.push(0);
+        }
+    }else if(array1.length < array2.length){
+        for(var i = array1.length; i < array2.length;i++){
+            array1.push(0);
+        }
+    }
+
+    for(let i = 0; i < array1.length; i++){
+        var soma = array1[i] + array2[i];
+        array.push(soma);
+    }
+
+    return array;
+}
+
+// Função para adicionar elementos de um array em outro array
+exports.questao10 = function(){
+    var vetorPilha = [1,2,3,4,5];
+    var vetorAdiciona = [6,7,8,9,10];
+
+    for(var i = 0; i < vetorAdiciona.length;i++){
+        vetorPilha.push(vetorAdiciona[i]);
+    }
+
+    return vetorPilha;
+}
