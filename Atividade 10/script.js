e6b93e73e955d1aebfe171432f76bb035c81ff3f
Marcos Vinicius Rodrigues Lima <marcos.viniciusk19@gmail.com>
2023-06-18 20:03:01 -0300

    Atividade 10 - CRUD API Externa
    Atualização funcionando!

--- Atividade 10/script.js
+++ Atividade 10/script.js
@@ -1,11 +1,20 @@
-function modal(type){
-    //alert(type);
+function modal(type, id){
+    document.getElementById("formModal").reset();
     var title = document.querySelector("#exampleModalLabel");
     title.textContent = type;
     var button = document.querySelector("#formButton");
     button.textContent = type;
     var crud = document.querySelector("#type");
     crud.value = type;
+    var idUser = document.querySelector("#id");
+    idUser.value = id;
+
+    if(type == "Atualizar"){
+        var nameUser = document.getElementById(("nameUser" + id)).value;
+        var emailUser = document.getElementById(("emailUser" + id)).value;
+        document.getElementById("formModal").name.value = nameUser;
+        document.getElementById("formModal").email.value = emailUser; 
+    }
 }
 
 function alerts(message, type, icon){
@@ -22,15 +31,20 @@ function alerts(message, type, icon){
     alertPlaceholder.append(wrapper)
 }
 
-async function request(config){
+async function request(config, type){
     return new Promise(function(resolve, reject){
         var xhr = new XMLHttpRequest();
         xhr.open(config.method, config.url, true);
 
         xhr.onload = function() {
             if (xhr.status === 200) {
-                console.log(JSON.parse(xhr.responseText).data);
-                resolve(JSON.parse(xhr.responseText).data);
+                if(type == 'Listar'){
+                    var response = JSON.parse(xhr.responseText).data;
+                }else if(type == 'Atualizar'){
+                    var response = JSON.parse(xhr.responseText);
+                }
+                console.log(response);
+                resolve(response);
             }else if(xhr.status === 201){
                 console.log(JSON.parse(xhr.responseText));
                 resolve(JSON.parse(xhr.responseText));
@@ -62,18 +76,22 @@ document.getElementById("formModal").addEventListener("submit", function(event) 
     }
     var email = formulario.get("email").trim();
     var type = formulario.get("type").trim();
+    var id = formulario.get("id").trim();
 
     var data = {
         'first_name': name[0],
         'last_name': name[1],
         'email': email,
-        'type': type
     }
 
     switch(type){
         case "Postar":
-            create(data);
+            create(data, type);
             break;
+        case "Atualizar":
+            data.id = parseInt(id);
+            console.log(id);
+            update(data, type);
     }
 
     var button = document.querySelector("#fecharModal");
@@ -100,14 +118,13 @@ function showResponse(response){
         row.classList.add('row');
         row.classList.add('border');
         row.classList.add('p-2');
-
         var inputs = [element.id, element.first_name + ' ' + element.last_name, element.email, 'bot'];
 
         row.innerHTML = 
         "<div class='col'>" +  inputs[0]
-        + "</div><div class='col'>" + inputs[1]
-        + "</div><div class='col'>" + inputs[2]
-        + "</div><div class='col'>" + "<button class='btn btn-warning me-2' onclick='modal(" + '"Atualizar"' + ")' data-bs-toggle='modal' data-bs-target='#exampleModal'><i class='fas fa-pencil fa-lg'></i></button><button class='btn btn-danger' onclick='modal(" + '"Deletar"' + ")'  data-bs-toggle='modal' data-bs-target='#exampleModal'><i class='fas fa-trash fa-lg'></i></button>"
+        + "</div><div class='col'>" + inputs[1] + "<input type='text' value='" + inputs[1] + "' id='nameUser" + inputs[0] + "' hidden>"
+        + "</div><div class='col'>" + inputs[2] + "<input type='text' value='" + inputs[2] + "' id='emailUser" + inputs[0] + "' hidden>"
+        + "</div><div class='col'>" + "<button class='btn btn-warning me-2' onclick='modal(" + '"Atualizar", ' + inputs[0] +  ")' data-bs-toggle='modal' data-bs-target='#exampleModal'><i class='fas fa-pencil fa-lg'></i></button><button class='btn btn-danger' onclick='modal(" + '"Deletar"' + ")'  data-bs-toggle='modal' data-bs-target='#exampleModal'><i class='fas fa-trash fa-lg'></i></button>"
         + "</div>";
         
         main.appendChild(row);
@@ -120,38 +137,46 @@ async function list(){
     const config = {
         'method': 'GET', 
         'url': 'https://reqres.in/api/users',
-        'type': 'Listar'
     };
 
     try {
-        var response = await request(config);
-        //console.log('Requisição concluída: ', response);
+        var response = await request(config, 'Listar');
         showResponse(response);
         alerts("Listagem concluída com sucesso!", "success", "check");
     } catch (error) {
         console.error('Erro na requisição:', error);
     }
-
-    request(config);
 }
 
-async function create(data){
+async function create(data, type){
     const config = {
         'method': 'POST', 
         'url': 'https://reqres.in/api/users',
-        'type': 'Postar',
         'data': data
     };
 
     try {
-        var response = await request(config);
+        var response = await request(config, type);
         //console.log('Requisição concluída: ', response);
         showResponse(response);
         alerts("Adição concluída com sucesso!", "success", "check");
     } catch (error) {
         console.error('Erro na requisição:', error);
     }
+}
 
-    request(config);
+async function update(data, type){
+    const config = {
+        'method': 'PUT', 
+        'url': 'https://reqres.in/api/users/' + data.id,
+        'data': data
+    };
 
+    try {
+        var response = await request(config, type);
+        showResponse(response);
+        alerts("Atualização concluída com sucesso!", "success", "check");
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+    }
 }
